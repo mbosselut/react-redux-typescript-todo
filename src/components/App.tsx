5c998c489b7334c83c8630c8267a324995724329
mbosselut <duchmol7@hotmail.com>
2020-01-20 10:15:49 +0100

    Add button to fetch todos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
 
-export class App extends Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+class _App extends Component<AppProps> {
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+
+  onButtonClick = (): void => {
+    this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo: Todo) => {
+      return <div key={todo.id}>{todo.title}</div>;
+    });
+  }
+
   render() {
-    return <div>Hi there !</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
+
+const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
+  return { todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
